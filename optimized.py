6c6165fcf2a5d2c986bca9066335aedbf6a15882
Matthew <delamotte.matthew@gmail.com>
2021-08-14 16:18:52 +0200

    json import

---
+++ optimized.py
@@ -0,0 +1,38 @@
+import json
+import itertools
+import time
+
+
+def get_json(json_file):
+    with open(json_file) as jsonfile:
+        data = json.load(jsonfile)
+    return data
+
+
+def get_combinations(data):
+    combinations_list = []
+    for L in range(0, len(data) + 1):
+        for combination in itertools.combinations(data, L):
+            combinations_list.append(combination)
+    return combinations_list
+
+
+def calc_price_combination(combinations):
+    result = []
+    for i in combinations:
+        print(i)
+        # result.append(data.get(i).get("price"))
+
+    return sum(result)
+
+
+def main():
+    data = get_json("data.json")
+    combinations = get_combinations(data)
+    calc_price_combination(combinations)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
