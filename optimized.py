903f0b564895f7fa2ef195e40533cf451a27b821
Matthew <delamotte.matthew@gmail.com>
2021-08-16 18:57:09 +0200

    Update optimized.py

--- optimized.py
+++ optimized.py
@@ -13,22 +13,49 @@ def get_combinations(data):
     combinations_list = []
     for L in range(0, len(data) + 1):
         for combination in itertools.combinations(data, L):
-            combinations_list.append(combination)
+            if calc_price_combination(combination, data) <= 500:
+                combinations_list.append(combination)
+
     return combinations_list
 
 
-def calc_price_combination(combinations):
+def calc_price_combination(combination, data):
     result = []
-    # for i in combinations:
-    # result.append(data.get(i).get("price"))
-    # return sum(result)
+    for i in combination:
+        result.append(data.get(i).get("price"))
+    return sum(result)
+
+
+data = {
+    "1": {"price": 20, "benefit": 5},
+    "2": {"price": 30, "benefit": 10},
+    "3": {"price": 50, "benefit": 15},
+    "4": {"price": 70, "benefit": 20},
+    "5": {"price": 60, "benefit": 17},
+    "6": {"price": 80, "benefit": 25},
+    "7": {"price": 22, "benefit": 7},
+    "8": {"price": 26, "benefit": 11},
+    "9": {"price": 48, "benefit": 13},
+    "10": {"price": 34, "benefit": 27},
+    "11": {"price": 42, "benefit": 17},
+    "12": {"price": 110, "benefit": 9},
+    "13": {"price": 38, "benefit": 23},
+    "14": {"price": 14, "benefit": 1},
+    "15": {"price": 18, "benefit": 3},
+    "16": {"price": 8, "benefit": 8},
+    "17": {"price": 4, "benefit": 12},
+    "18": {"price": 10, "benefit": 14},
+    "19": {"price": 24, "benefit": 21},
+    "20": {"price": 114, "benefit": 18},
+}
 
 
 def main():
-    data = get_json("data.json")
-    print(data[0].get("price"))
+    # data = get_json("data.json")
     combinations = get_combinations(data)
-    # calc_price_combination(combinations)
+
+    # for i in data:
+    #     print(data.get(i).get("price"))
 
 
 if __name__ == "__main__":
